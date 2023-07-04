b8a476fa37a3e81437fe4617e5378a5be396b5a7
nahuel220 <129806290+nahuel220@users.noreply.github.com>
2023-07-03 21:22:07 -0300

    Añadi archivos de Web Client

---
+++ 2 Web Client/index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>MoyanoStuff</title>
+</head>
+<body>
+    <header class="cabeza">
+        <img src="fries.png">
+        <h1>Welcome to MoyanoStuff</h1>
+    </header>
+    <div class="pedido">
+        <form>
+            <input type="submit" name="1" value="hamburguesa simple">
+            <input type="submit" name="2" value="hamburguesa mediana">
+            <input type="submit" name="3" value="hamburguesa grande">
+            <input type="submit" name="4" value="papas chicas">
+            <input type="submit" name="5" value="papas grandes">
+            <input type="submit" name="6" value="cocacola">
+            <input type="submit" name="7" value="sprite">
+        </form>
+    </div>
+    <div class="historial">
+        <form>
+            <center><h2>Pedidos ahora</h2></center>
+            <span class="seg"><p>-Hamburguesa triple   $900</p></span>
+            <center><input type="submit" name="DETENER" value="DETENER"></center>
+        </form>
+        </div>
+    </section>
+</body>
+</html>
+
+<?php
+
+echo "holaholahola";
+
+
+?>
